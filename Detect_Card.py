5926c5a3a1ce8437df5957a094077a5547cfda8a
0swan <owen_swanson@mis-munich.de>
2025-05-21 10:12:49 +0200

    Update Detect_Card.py
    hi i like men

--- Detect_Card.py
+++ Detect_Card.py
@@ -28,6 +28,8 @@ def preprocess_frame(frame, target_size=(224, 224)):
     img = cv2.resize(frame, target_size)
     img = img.astype('float32') / 255.0
     img = np.expand_dims(img, axis=0)
+    img = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+    img = (img / 127.5) - 1
     return img
 
 # Start webcam capture
@@ -46,12 +48,16 @@ while True:
     # Preprocess and predict
     processed = preprocess_frame(center_crop)
     prediction = model.predict(processed)
-    class_index = np.argmax(prediction[0])
-    label = labels[class_index]
-    value = blackjack_values[label]
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    value = blackjack_values[class_name[2:]
+    confidence_score = prediction[0][index]
+    confidence_score = str(np.round(confidence_score * 100))[:-2]
 
     # Display result
     cv2.putText(frame, f"{label.upper()} (Value: {value})", (30, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                1.2, (0, 255, 0), 3, cv2.LINE_AA)
+    cv2.putText(frame, f"{label.upper()} (Confidence: {confidence_score})", (30, 20), cv2.FONT_HERSHEY_SIMPLEX,
                 1.2, (0, 255, 0), 3, cv2.LINE_AA)
     cv2.rectangle(frame, (w//4, h//4), (w*3//4, h*3//4), (255, 0, 0), 2)
     cv2.imshow("Card Detector", frame)
